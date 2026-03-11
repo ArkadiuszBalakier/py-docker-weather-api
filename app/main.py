bcc52356bcd62cd5bfc5e9680b2519c90f7bdcef
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-03-11 13:07:40 +0100

    Implement weather fetching functionality and update dependencies

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    load_dotenv()
+
+    API_KEY = os.getenv("API_KEY")
+    URL = os.getenv("URL")
+    FILTERING = os.getenv("FILTERING")
+
+    if not all([API_KEY, URL, FILTERING]):
+        print("Missing environment variables. Check -e flags.")
+        return
+
+    response = requests.get(f"{URL}/current.json?key={API_KEY}&q={FILTERING}")
+    print(response.json())
 
 
 if __name__ == "__main__":
